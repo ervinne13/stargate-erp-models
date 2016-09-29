23b892d3e3a14e1adadcf9a349e77d74aa84c079
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2016-09-29 12:33:29 +0800

    Added test model

---
+++ Stargate/TestModel.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TestModel
+ *
+ * @author ervinne
+ */
+class TestModel {
+    //Does nothing, just for testing
+}
